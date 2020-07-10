132b2a77a1c775577369323af724f95ebd2ec945
kirill <kirill.prokofiev@intel.com>
2020-07-10 11:48:12 +0300

    main structure released acc=0.71

---
+++ VGG.py
@@ -0,0 +1,57 @@
+''' Implementation of VGG class Neural Network for cifar10'''
+import torch
+import numpy as np
+import torch.nn as nn
+
+# architecture is looked like: image -> 64*64, M, 128*128, M, 256*256*256, M, 512*512*512, M, FC, FC, FC
+VGG_types = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 
+          512, 512, 512, 512, 'M'],
+}
+
+class VGG(torch.nn.Module):
+    def __init__(self, in_channels=3, num_classes=10):
+        super().__init__()
+        self.in_channels = in_channels
+        self.conv_layers = self.create_conv_layers(VGG_types['A'])
+        # FC layers
+        self.fcs = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+
+        x = x.reshape(x.shape[0], -1)
+        x = self.fcs(x)
+
+        return x
+
+    def create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+
+        for x in architecture:
+            if type(x) == int:
+                out_channels = x
+                layers += [nn.Conv2d(in_channels = in_channels, out_channels = out_channels, 
+                                    kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+                            nn.ReLU()]
+                in_channels = x
+            elif x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))]
+        return nn.Sequential(*layers)
+
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# model = VGG(in_channels=3, num_classes=10).to(device)
+# x = torch.randn(1, 3, 32, 32).to(device)
+# print(model(x).shape, device)
---
+++ main.py
@@ -0,0 +1,32 @@
+''' train and validate model '''
+import torch
+import torchvision.datasets
+import VGG
+import trainVGG
+
+
+CIFAR_train = torchvision.datasets.CIFAR10('./', download=True, train=True)
+CIFAR_test = torchvision.datasets.CIFAR10('./', download=True, train=False)
+
+X_train = torch.FloatTensor(CIFAR_train.data)
+y_train = torch.LongTensor(CIFAR_train.targets)
+X_test = torch.FloatTensor(CIFAR_test.data)
+y_test = torch.LongTensor(CIFAR_test.targets)
+
+# normilize ant permutate data to suit torch
+X_train /= 255.
+X_test /= 255.
+X_train = X_train.permute(0, 3, 1, 2)
+X_test = X_test.permute(0, 3, 1, 2)
+print(f'X_train shape: {X_train.shape}',
+    f'X_test shape: {X_test.shape}',
+    f'y_train shape: {y_train.shape}',
+    f'y_test shape: {y_test.shape}'
+)
+
+def main():
+    accuracies = {}
+    losses = {}
+    accuracies['VGG'], losses['VGG'] = trainVGG.train2(VGG.VGG(), X_train, y_train, X_test, y_test)
+
+main()
